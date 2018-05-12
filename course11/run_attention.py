ed50a533a7af937f0218293a13fe87abf2a2ab58
zyc <zhengyuanchun92@gmail.com>
2018-05-12 22:37:48 +0800

    fix course11 bug

--- course11/run_attention.py
+++ course11/run_attention.py
@@ -2,10 +2,9 @@
 from __future__ import print_function
 from keras.preprocessing import sequence
 from keras.datasets import imdb
-from attention_keras import Attention,Position_Embedding
-
+from attention_keras import Attention, Position_Embedding
 max_features = 20000
-maxlen = 80
+maxlen = 150
 batch_size = 32
 
 print('Loading data...')
@@ -24,11 +23,11 @@ from keras.layers import *
 
 S_inputs = Input(shape=(None,), dtype='int32')
 embeddings = Embedding(max_features, 128)(S_inputs)
-embeddings = Position_Embedding()(embeddings) #增加Position_Embedding能轻微提高准确率
+embeddings = Position_Embedding()(embeddings) # 增加Position_Embedding能轻微提高准确率
 O_seq = Attention(8,16)([embeddings,embeddings,embeddings])
 O_seq = GlobalAveragePooling1D()(O_seq)
 O_seq = Dropout(0.5)(O_seq)
-outputs = Dense(1, activation='relu')(O_seq)
+outputs = Dense(1, activation='sigmoid')(O_seq)
 
 model = Model(inputs=S_inputs, outputs=outputs)
 # try using different optimizers and different optimizer configs
